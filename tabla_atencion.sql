03062a16a0c2fdea74eb5f1be271e2f6c4729079
Lizandro <ESTANISLAO.GOMEZ@correoaiep.cl>
2024-05-05 22:56:31 -0400

    fn

---
+++ tabla_atencion.sql
@@ -0,0 +1,14 @@
+INSERT INTO Atencion (fecha, Comentarios, id_valor, id_personal, id_cargo, id_contacto, id_diagonostico, id_tramiento, id_receta)
+VALUES
+('2024-04-01', 'El paciente se queja de dolores en el pecho.', NULL, 1, 1, 1, 1, 1, 1),
+('2024-04-02', 'El paciente presenta fiebre y tos.', NULL, 2, 2, 2, 2, 2, 2),
+('2024-04-03', 'El paciente tiene dificultades para respirar.', NULL, 3, 3, 3, 3, 3, 3),
+('2024-04-04', 'El paciente reporta dolor de cabeza constante.', NULL, 4, 4, 4, 4, 4, 4),
+('2024-04-05', 'El paciente se siente mareado y desorientado.', NULL, 5, 5, 5, 5, 5, 5),
+('2024-04-06', 'El paciente ha experimentado pérdida de apetito.', NULL, 6, 6, 6, 6, 6, 6),
+('2024-04-07', 'El paciente ha notado sangrado al cepillarse los dientes.', NULL, 7, 7, 7, 7, 7, 7),
+('2024-04-08', 'El paciente presenta inflamación en las articulaciones.', NULL, 8, 8, 8, 8, 8, 8),
+('2024-04-09', 'El paciente se queja de visión borrosa.', NULL, 9, 9, 9, 9, 9, 9),
+('2024-04-10', 'El paciente ha experimentado fatiga extrema.', NULL, 10, 10, 10, 10, 10, 10),
+('2024-04-11', 'El paciente ha tenido dificultades para conciliar el sueño.', NULL, 11, 11, 11, 11, 11, 11),
+('2024-04-12', 'El paciente se queja de dolor abdominal.', NULL, 12, 12, 12, 12, 12, 12);
