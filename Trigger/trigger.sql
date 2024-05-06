58682ebcce27f4b91f75b17d515f52870d9e7566
Lizandro <ESTANISLAO.GOMEZ@correoaiep.cl>
2024-05-05 23:59:24 -0400

    trigger control personal

--- Trigger/trigger.sql
+++ Trigger/trigger.sql
@@ -1,7 +1,15 @@
 
+--creacion de tabla de control de personal  
 
+create table control(
+	usario varchar (30),
+	fecha date,
+	accion varchar(50)
+);
 
 Select * from control
+
+-- creacion de triggees de nuevo ingreso al personal
 
 Create or alter trigger tgr_nuevos_ingresos
 on personal
